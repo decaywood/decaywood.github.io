bced86fcfb23a8c465668465cec98d7cc49da7f8
decaywood <decaywood@qq.com>
2016-01-04 13:15:25 +0800

    animation

--- js/decaywood-blog.js
+++ js/decaywood-blog.js
@@ -152,7 +152,33 @@ $(document).ready(function () {
     })
 });
 
+/* Animation */
+$(document).ready(function () {
 
+    $('body').addClass("animsition");
+    $('a:not(#night-mode):not([href^=#])').addClass("animsition-link");
+
+    $(".animsition").animsition({
+        inClass: 'fade-in-down',
+        outClass: 'fade-out-down',
+        inDuration: 300,
+        outDuration: 300,
+        linkElement: '.animsition-link',
+        loading: true,
+        loadingClass: 'animsition-loading',
+        loadingInner: '', // e.g '<img src="loading.svg" />'
+        timeout: false,
+        timeoutCountdown: 5000,
+        onLoadEvent: true,
+        browser: ['animation-duration', '-webkit-animation-duration'],
+        overlay: false,
+        overlayClass: 'animsition-overlay-slide',
+        overlayParentElement: 'body',
+        transition: function (url) {
+            window.location.href = url;
+        }
+    });
+});
 
 /*  generate TOC */
 if($('#toc').length > 0) {
