b5c0b7fabd05305244e73813abfb32affa811128
decaywood <decaywood@qq.com>
2016-01-16 15:05:50 +0800

    update readme.md retranslate article, update url, change the img load func

--- README.md
+++ README.md
@@ -2,14 +2,23 @@
 
 ## 为了方便写的自动化脚本：
 
-* [jekyll_install.sh](https://github.com/decaywood/decaywood.github.io/blob/master/jekyll_install.sh)一键安装环境
+* [jekyll_install.sh](https://github.com/decaywood/decaywood.github.io/blob/master/shell/jekyll_install.sh)一键安装环境
 
 ```bash
 sh jekyll_install.sh
 ```
 
-* [catalogGenerator.py](https://github.com/decaywood/decaywood.github.io/blob/master/catalogGenerator.py)自动生成markdown目录脚本
+* [catalogGenerator.py](https://github.com/decaywood/decaywood.github.io/blob/master/shell/catalogGenerator.py)自动生成markdown目录脚本,目前已实现javascript自动生成目录，此脚本弃用
 
 ```bash
 python catalogGenerator.py _posts/your_file.md
+```
+* [PicRename.java](https://github.com/decaywood/decaywood.github.io/blob/master/shell/PicRename.java) 重命名指定文件夹内的jpg文件(命名后：1.jpg 2.jpg ... n.jpg)，用于随机请求背景图片
+
+```bash
+javac PicRename.java
+mv PicRename.class your_pic_dir
+cd your_pic_dir
+java PicRename
+cd change
 ```
---
+++ shell/PicRename.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * @author: decaywood
+ * @date: 2016/1/16 9:23.
+ */
+public class PicRename {
+
+    private static final LongAdder longAdder = new LongAdder();
+
+    private static long increament() {
+        longAdder.increment();
+        return longAdder.intValue();
+    }
+
+    private static void reset() {
+        longAdder.reset();
+    }
+
+    private synchronized static void nioTransferCopy(File source, File target) {
+        try (FileInputStream inStream = new FileInputStream(source);
+             FileOutputStream outStream = new FileOutputStream(target);
+             FileChannel in = inStream.getChannel();
+             FileChannel out = outStream.getChannel()) {
+
+            in.transferTo(0, in.size(), out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String path = PicRename.class.getClassLoader().getResource(".").getPath();
+        File file = new File(path);
+        String split = file.getPath().contains("/") ? "/" : "\\";
+        File folder = new File(file.getPath().trim() + split + "changed");
+        if(!folder.exists()) folder.mkdir();
+        String outputPath = folder.getPath() + split;
+        Arrays.stream(file.listFiles())
+                .parallel()
+                .filter(x -> x.isFile() && x.getName().contains(".jpg"))
+                .forEach(x -> nioTransferCopy(x, new File(outputPath + increament() + ".jpg")));
+        reset();
+    }
+}
