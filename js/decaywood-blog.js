380d42c9e29d102f2cce61fbc64373d953d9f26d
decaywood <decaywood@qq.com>
2016-01-29 19:24:29 +0800

    removed Amaze UI

--- js/decaywood-blog.js
+++ js/decaywood-blog.js
@@ -1,9 +1,3 @@
-/*!
- * Clean Blog v1.0.0 (http://startbootstrap.com)
- * Copyright 2015 Start Bootstrap
- * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
- */
-
 window.onload = function () {
     window.console.info("window loaded");
     var body = $('body');
@@ -183,16 +177,63 @@ if ($('#tag_cloud').length > 0) {
 /* Night Mode */
 $(function () {
 
-    /* Amaze UI cookie API */
-    var store = $.AMUI.store;
+    var cookie_opt = function (key, value, options) {
+
+        // value can be a function
+
+        function read(s, converter) {
+            var value = decodeURIComponent(s);decodeURIComponent(s)
+            return $.isFunction(converter) ? converter(value) : value;
+        }
+
+        // Write
+
+        if (value !== undefined && !$.isFunction(value)) {
+            options = $.extend({}, options);
+
+            var millisecScale = 864e+5; // day to millisec
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setTime(+t + days * millisecScale);
+            }
+            return (document.cookie = [
+                encodeURIComponent(key), '=', encodeURIComponent(String(value)),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path    ? '; path=' + options.path : '',
+                options.domain  ? '; domain=' + options.domain : '',
+                options.secure  ? '; secure' : ''
+            ].join(''));
+        }
+
+        // Read
+
+        var result = "";
+
+        var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+
+        for (var i = 0, l = cookies.length; i < l; i++) {
+            var parts = cookies[i].split('=');
+            var name = decodeURIComponent(parts.shift());
+            var cookie = parts.join('=');
+            if (key && key === name) {
+                // If second argument (value) is a function it's a converter...
+                result = read(cookie, value);
+                break;
+            }
+        }
+        return result;
+    };
+
 
     var loadMode = function (mode, text) {
         theme.attr('href', cssPath + mode);
         button.text(text);
-        if (store.enabled) {
-            store.set('mode', mode);
-            store.set('text', text);
-        }
+        if (window.navigator.cookieEnabled) {
+            cookie_opt('mode', mode, { expires: 7 });
+            cookie_opt('text', text, { expires: 7 });
+        } else console.info('cookie is not available')
     };
 
     var button = $('#night-mode');
@@ -201,11 +242,9 @@ $(function () {
     theme.mode = 'daily-mode.min.css';
     button.btnText = 'Night Mode';
 
-    /* Amaze UI cookie API */
-
-    if (store.enabled) {
-        var localMode = store.get('mode');
-        var localText = store.get('text');
+    if (window.navigator.cookieEnabled) {
+        var localMode = cookie_opt('mode');
+        var localText = cookie_opt('text');
         if (localMode && localText) {
             theme.mode = localMode;
             button.btnText = localText;
