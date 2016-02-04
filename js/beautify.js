ff71ec37b041e941d7d8b326369b9346fa89bbb3
decaywood <decaywood@qq.com>
2016-02-04 21:30:12 +0800

    refactor the animation

--- js/beautify.js
+++ js/beautify.js
@@ -1,235 +1,241 @@
 /*
  * optimize the animations of browser
  */
-$(function() {
-    var lastTime = 0;
-    var vendors = ["ms", "moz", "webkit", "o"];
-    /*
-     * retrieve the browser's current implementation of
-     * requestAnimationFrame and cancelAnimationFrame
-     */
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+"RequestAnimationFrame"];
-        window.cancelAnimationFrame = window[vendors[x]+"CancelAnimationFrame"]
-            || window[vendors[x]+"CancelRequestAnimationFrame"];
-    }
-    /*
-     * back off the func to 'setTimeout' if current browser
-     * doesn't support requestAnimationFrame
-     */
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    /*
-     * back off the func to 'setTimeout' if current browser
-     * doesn't support cancelAnimationFrame
-     */
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-});
-
-/*----------------------------  Reactive Background  ----------------------------*/
-
-$(function () {
-
-    var width, height, reactiveBg, canvas, ctx, points, target, _reactive = true;
-
-    // Main
-    initHeader();
-    initAnimation();
-    addListeners();
-
-    function initHeader() {
-
-        reactiveBg = document.getElementById("reactive-bg");
-
-        width = reactiveBg.offsetWidth;
-        height = reactiveBg.offsetHeight;
-
-        console.info(width);
-        console.info(height);
-
-        target = {x: width / 2, y: height / 2};
-
-        reactiveBg.style.height = height + "px";
-
-        canvas = document.getElementById("reactive-bg-canvas");
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext("2d");
-
-        // create points
-        points = [];
-        for (var x = 0; x < width; x = x + width / 20) {
-            for (var y = 0; y < height; y = y + height / 20) {
-                var px = x + Math.random() * width / 20;
-                var py = y + Math.random() * height / 20;
-                var p = {x: px, originX: px, y: py, originY: py};
-                points.push(p);
-            }
+if ($('#reactive-bg').length > 0) {
+    $(function () {
+        var lastTime = 0;
+        var vendors = ["ms", "moz", "webkit", "o"];
+        /*
+         * retrieve the browser's current implementation of
+         * requestAnimationFrame and cancelAnimationFrame
+         */
+        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
+            window.cancelAnimationFrame = window[vendors[x] + "CancelAnimationFrame"]
+                || window[vendors[x] + "CancelRequestAnimationFrame"];
         }
+        /*
+         * back off the func to 'setTimeout' if current browser
+         * doesn't support requestAnimationFrame
+         */
+        if (!window.requestAnimationFrame)
+            window.requestAnimationFrame = function (callback) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function () {
+                        callback(currTime + timeToCall);
+                    },
+                    timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+        /*
+         * back off the func to 'setTimeout' if current browser
+         * doesn't support cancelAnimationFrame
+         */
+        if (!window.cancelAnimationFrame)
+            window.cancelAnimationFrame = function (id) {
+                clearTimeout(id);
+            };
+    });
 
-        // for each point find the 5 closest points
-        for (var i = 0; i < points.length; i++) {
-            var closest = [];
-            var p1 = points[i];
-            for (var j = 0; j < points.length; j++) {
-                var p2 = points[j];
-                if (!(p1 == p2)) {
-                    var placed = false;
-                    for (var k = 0; k < 5; k++) {
-                        if (!placed) {
-                            if (closest[k] == undefined) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
+    /*----------------------------  Reactive Background  ----------------------------*/
 
-                    for (var k = 0; k < 5; k++) {
-                        if (!placed) {
-                            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
+    $(function () {
+
+        var width, height, reactiveBg, canvas, ctx, points, target, _reactive = true;
+
+        // Main
+        initHeader();
+        initAnimation();
+        addListeners();
+
+        function initHeader() {
+
+            reactiveBg = document.getElementById("reactive-bg");
+
+            width = reactiveBg.offsetWidth;
+            height = reactiveBg.offsetHeight;
+
+            console.info(width);
+            console.info(height);
+
+            target = {x: width / 2, y: height / 2};
+
+            reactiveBg.style.height = height + "px";
+
+            canvas = document.getElementById("reactive-bg-canvas");
+            canvas.width = width;
+            canvas.height = height;
+            ctx = canvas.getContext("2d");
+
+            // create points
+            points = [];
+            for (var x = 0; x < width; x = x + width / 20) {
+                for (var y = 0; y < height; y = y + height / 20) {
+                    var px = x + Math.random() * width / 20;
+                    var py = y + Math.random() * height / 20;
+                    var p = {x: px, originX: px, y: py, originY: py};
+                    points.push(p);
                 }
             }
-            p1.closest = closest;
-        }
 
-        // assign a circle to each point
-        for (var i = 0; i < points.length; i++) {
-            points[i].circle = new Circle(points[i], 2 + Math.random() * 2, "rgba(255,255,255,0.3)");
-        }
-    }
-
-    // Event handling
-    function addListeners() {
-        if (!("ontouchstart" in window)) {
-            window.addEventListener("mousemove", mouseMove);
-        }
-        window.addEventListener("scroll", scrollCheck);
-        window.addEventListener("resize", resize);
-    }
-
-    function mouseMove(e) {
-        var posX = 0;
-        var posY = 0;
-        if (e.pageX || e.pageY) {
-            posX = e.pageX;
-            posY = e.pageY;
-        }
-        else if (e.clientX || e.clientY) {
-            posX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posX;
-        target.y = posY;
-    }
-
-    function scrollCheck() {
-        _reactive = document.body.scrollTop <= height;
-    }
-
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        reactiveBg.style.height = height + "px";
-        canvas.width = width;
-        canvas.height = height;
-    }
-
-    // animation
-    function initAnimation() {
-        animate();
-        for (var i = 0; i < points.length; i++) {
-            shiftPoint(points[i]);
-        }
-    }
-
-    function animate() {
-        if (_reactive) {
-            ctx.clearRect(0, 0, width, height);
+            // for each point find the 5 closest points
             for (var i = 0; i < points.length; i++) {
-                // detect points in range
-                if (Math.abs(getDistance(target, points[i])) < 4000) {
-                    points[i].active = 0.3;
-                    points[i].circle.active = 0.6;
-                } else if (Math.abs(getDistance(target, points[i])) < 20000) {
-                    points[i].active = 0.1;
-                    points[i].circle.active = 0.3;
-                } else if (Math.abs(getDistance(target, points[i])) < 40000) {
-                    points[i].active = 0.02;
-                    points[i].circle.active = 0.1;
-                } else {
-                    points[i].active = 0;
-                    points[i].circle.active = 0;
+                var closest = [];
+                var p1 = points[i];
+                for (var j = 0; j < points.length; j++) {
+                    var p2 = points[j];
+                    if (!(p1 == p2)) {
+                        var placed = false;
+                        for (var k = 0; k < 5; k++) {
+                            if (!placed) {
+                                if (closest[k] == undefined) {
+                                    closest[k] = p2;
+                                    placed = true;
+                                }
+                            }
+                        }
+
+                        for (var k = 0; k < 5; k++) {
+                            if (!placed) {
+                                if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                    closest[k] = p2;
+                                    placed = true;
+                                }
+                            }
+                        }
+                    }
                 }
+                p1.closest = closest;
+            }
 
-                drawLines(points[i]);
-                points[i].circle.draw();
+            // assign a circle to each point
+            for (var i = 0; i < points.length; i++) {
+                points[i].circle = new Circle(points[i], 2 + Math.random() * 2, "rgba(255,255,255,0.3)");
             }
         }
-        requestAnimationFrame(animate);
-    }
 
-    function shiftPoint(p) {
-        TweenLite.to(p, 1 + Math.random(), {
-            x: p.originX - 50 + Math.random() * 100,
-            y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut,
-            onComplete: function () {
-                shiftPoint(p);
+        // Event handling
+        function addListeners() {
+            if (!("ontouchstart" in window)) {
+                window.addEventListener("mousemove", mouseMove);
             }
-        });
-    }
-
-    // Canvas manipulation
-    function drawLines(p) {
-        if (!p.active) return;
-        for (var i = 0; i < p.closest.length; i++) {
-            ctx.beginPath();
-            ctx.moveTo(p.x, p.y);
-            ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = "rgba(156,217,249," + p.active + ")";
-            ctx.stroke();
+            window.addEventListener("scroll", scrollCheck);
+            window.addEventListener("resize", resize);
         }
-    }
 
-    function Circle(pos, rad, color) {
-        var _this = this;
+        function mouseMove(e) {
+            var posX = 0;
+            var posY = 0;
+            if (e.pageX || e.pageY) {
+                posX = e.pageX;
+                posY = e.pageY;
+            }
+            else if (e.clientX || e.clientY) {
+                posX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+                posY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+            }
+            posX = posX - reactiveBg.offsetLeft;
+            posY = posY - reactiveBg.offsetTop;
+            target.x = posX;
+            target.y = posY;
+        }
 
-        // constructor
-        (function () {
-            _this.pos = pos || null;
-            _this.radius = rad || null;
-            _this.color = color || null;
-        })();
+        function scrollCheck() {
+            _reactive = document.body.scrollTop <= height;
+        }
 
-        this.draw = function () {
-            if (!_this.active) return;
-            ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = "rgba(156,217,249," + _this.active + ")";
-            ctx.fill();
-        };
-    }
+        function resize() {
+            width = window.innerWidth;
+            height = window.innerHeight;
+            reactiveBg.style.height = height + "px";
+            canvas.width = width;
+            canvas.height = height;
+        }
 
-    // Util
-    function getDistance(p1, p2) {
-        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-    }
+        // animation
+        function initAnimation() {
+            animate();
+            for (var i = 0; i < points.length; i++) {
+                shiftPoint(points[i]);
+            }
+        }
 
-});
+        function animate() {
+            if (_reactive) {
+                ctx.clearRect(0, 0, width, height);
+                for (var i = 0; i < points.length; i++) {
+                    // detect points in range
+                    if (Math.abs(getDistance(target, points[i])) < 4000) {
+                        points[i].active = 0.3;
+                        points[i].circle.active = 0.6;
+                    } else if (Math.abs(getDistance(target, points[i])) < 20000) {
+                        points[i].active = 0.1;
+                        points[i].circle.active = 0.3;
+                    } else if (Math.abs(getDistance(target, points[i])) < 40000) {
+                        points[i].active = 0.02;
+                        points[i].circle.active = 0.1;
+                    } else {
+                        points[i].active = 0;
+                        points[i].circle.active = 0;
+                    }
+
+                    drawLines(points[i]);
+                    points[i].circle.draw();
+                }
+            }
+            requestAnimationFrame(animate);
+        }
+
+        function shiftPoint(p) {
+            TweenLite.to(p, 1 + Math.random(), {
+                x: p.originX - 50 + Math.random() * 100,
+                y: p.originY - 50 + Math.random() * 100, ease: Circ.easeInOut,
+                onComplete: function () {
+                    shiftPoint(p);
+                }
+            });
+        }
+
+        // Canvas manipulation
+        function drawLines(p) {
+            if (!p.active) return;
+            for (var i = 0; i < p.closest.length; i++) {
+                ctx.beginPath();
+                ctx.moveTo(p.x, p.y);
+                ctx.lineTo(p.closest[i].x, p.closest[i].y);
+                ctx.strokeStyle = "rgba(156,217,249," + p.active + ")";
+                ctx.stroke();
+            }
+        }
+
+        function Circle(pos, rad, color) {
+            var _this = this;
+
+            // constructor
+            (function () {
+                _this.pos = pos || null;
+                _this.radius = rad || null;
+                _this.color = color || null;
+            })();
+
+            this.draw = function () {
+                if (!_this.active) return;
+                ctx.beginPath();
+                ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+                ctx.fillStyle = "rgba(156,217,249," + _this.active + ")";
+                ctx.fill();
+            };
+        }
+
+        // Util
+        function getDistance(p1, p2) {
+            return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+        }
+
+    });
+}
 
 /* Animation */
 $(function () {
@@ -240,7 +246,7 @@ $(function () {
         var body = $('body');
         body.removeAttr("id"); // id = "body-hidden"
         var introHeader = $(".intro-header");
-        if(introHeader.css("background-image") == "none") {
+        if (introHeader.css("background-image") == "none") {
             introHeader.css("background-image", "url(" + headerImg + ")");
         }
 
@@ -268,7 +274,7 @@ $(function () {
         timeout: false,
         timeoutCountdown: 5000,
         onLoadEvent: true,
-        browser: ['animation-duration', '-webkit-animation-duration','-o-animation-duration'],
+        browser: ['animation-duration', '-webkit-animation-duration', '-o-animation-duration'],
         overlay: false,
         overlayClass: 'animsition-overlay-slide',
         overlayParentElement: 'body',
@@ -282,7 +288,7 @@ $(function () {
  * add line under every article title
  */
 $(function () {
-    $("h2:not(.subheading):not(.post-title)").each(function(){
+    $("h2:not(.subheading):not(.post-title)").each(function () {
         $(this).append("<hr/>");
     })
 });
@@ -295,7 +301,8 @@ $(function () {
         // value can be a function
 
         function read(s, converter) {
-            var value = decodeURIComponent(s);decodeURIComponent(s)
+            var value = decodeURIComponent(s);
+            decodeURIComponent(s)
             return $.isFunction(converter) ? converter(value) : value;
         }
 
@@ -313,9 +320,9 @@ $(function () {
             return (document.cookie = [
                 encodeURIComponent(key), '=', encodeURIComponent(String(value)),
                 options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-                options.path    ? '; path=' + options.path : '',
-                options.domain  ? '; domain=' + options.domain : '',
-                options.secure  ? '; secure' : ''
+                options.path ? '; path=' + options.path : '',
+                options.domain ? '; domain=' + options.domain : '',
+                options.secure ? '; secure' : ''
             ].join(''));
         }
 
@@ -344,8 +351,8 @@ $(function () {
         theme.attr('href', cssPath + mode);
         button.text(text);
         if (window.navigator.cookieEnabled) {
-            cookie_opt('mode', mode, { expires: 100 });
-            cookie_opt('text', text, { expires: 100 });
+            cookie_opt('mode', mode, {expires: 100});
+            cookie_opt('text', text, {expires: 100});
         } else console.info('cookie is not available')
     };
 
